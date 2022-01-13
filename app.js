ecd4ded511ae309720c1f4bd893169b6f270968b
Hubert Bukala <hubertbukala@icloud.com>
2022-01-13 21:59:47 +0100

    added functionality to the start game button

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 const startGameBtn = document.getElementById('start-game-btn');
+
+const start = function () {
+    console.log("Game is starting...");
+};
+
+startGameBtn.addEventListener('click', start);
+
